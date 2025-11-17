db32c0afc1d477d58e7798b4224ba4d9f31afc15
Yakub <murcja812@gmail.com>
2025-11-17 14:42:43 +0100

    fix: csv encoding in excel

--- extract50csv.py
+++ extract50csv.py
@@ -10,12 +10,14 @@ df = pd.read_csv(
     input_csv,
     sep=";",
     nrows=50,
+    encoding="utf-8"
 )
 
 df.to_csv(
     output_csv,
     sep=";",
     index=False,
+    encoding="utf-8-sig"
 )
 
 print("Done. Exported first 50 rows.")
